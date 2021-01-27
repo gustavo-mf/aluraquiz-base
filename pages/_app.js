c6f352cd0103e8d516c1a565ffaa2f9a0d795b2c
Gustavo-mf <gustavofreitas@frigelar.com.br>
2021-01-26 21:37:31 -0300

    Codigo final da primeira aula de imersão react

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,47 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme;
+
+//primary
+//#00695f
+//#009688
+//#33ab9f
+
+//secondary
+//#4615b2
+//#651fff
+//#834bff
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle /> 
         <Component {...pageProps} />
       </ThemeProvider>
     </>
